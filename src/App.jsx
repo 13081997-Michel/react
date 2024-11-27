d800bc6025f739729dc9d38bf194b30ed8976a18
13081997-Michel <yoshimura1997@hotmail.com>
2024-11-27 08:53:58 +0000

    my website

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Header } from './components/Header';
+
+import {Header} from './components/Header'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Signup} from './pages/Signup'
+import {Routes,Route} from 'react-router-dom'
+import { Client } from 'appwrite'
 
 
 function App() {
+  const client = new Client();
+  client.setProject('6746c346000916620394');
+
+  
 
   return (
     <>
-    <Header text="Wines" />
+      <Header text="Wine" />
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path='/register' element={ <Signup/> } />
+      </Routes>
+      
     </>
   )
 }
